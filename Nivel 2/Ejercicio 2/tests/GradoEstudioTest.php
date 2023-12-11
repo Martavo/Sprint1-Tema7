c9574c1c379e5cea9703885748d01ce746915289
Marta Villalba <marta.villalba.orta@gmail.com>
2023-12-11 15:45:10 +0100

    Ejercicio 2 revisado, funciona

--- Nivel 2/Ejercicio 2/tests/GradoEstudioTest.php
+++ Nivel 2/Ejercicio 2/tests/GradoEstudioTest.php
@@ -1,27 +1,21 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class GradoEstudioTest extends TestCase
-{
+class GradoEstudioTest extends TestCase {
     /**
      * @dataProvider gradoProvider
      */
-    public function testGrado($nota)
-    {
+    public function testGrado($nota, $expectedResult) {
         $estudiante = new GradoEstudio($nota);
-        $this->assertEquals("Primera división", $estudiante->calcularGradoEstudio());
-        $this->assertEquals("Segunda división", $estudiante->calcularGradoEstudio());
-        $this->assertEquals("Tercera división", $estudiante->calcularGradoEstudio());
-        $this->assertEquals("Pepetir", $estudiante->calcularGradoEstudio());
+        $this->assertEquals($expectedResult, $estudiante->calcularGradoEstudio());
     }
 
-    public function gradoProvider()
-    {
+    public function gradoProvider() { //Se indica los valores que se revisan y las espectativas de mensajes correspondientes
         return [
-            [72],
-            [46],
-            [36],
-            [22],
+            [72, "Corresponde a la primera división"],
+            [46, "Corresponde a la segunda división"],
+            [36, "Corresponde a la tercera división"],
+            [22, "Tienes que volver a repetir"],
         ];
     }
 }
