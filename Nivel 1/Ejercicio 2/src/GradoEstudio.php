881c73fe12137b82c31d80c03f96a1e875f8a962
Marta Villalba <marta.villalba.orta@gmail.com>
2023-12-12 09:03:33 +0100

    Revisión final

--- Nivel 1/Ejercicio 2/src/GradoEstudio.php
+++ Nivel 1/Ejercicio 2/src/GradoEstudio.php
@@ -1,19 +1,17 @@
 <?php
-class GradoEstudio{
+class GradoEstudio {
+    public function __construct(private int $nota) {}
 
-    public function __construct(private int $nota){}
-
-    function CalcularGrado_Estudio($nota):string{
-        if ($nota >= 60) {
+    public function CalcularGrado_Estudio(): string {
+        if ($this->nota >= 60) {
             return "Corresponde a la primera división";
-        } elseif ($nota >= 45) {
+        } elseif ($this->nota >= 45) {
             return "Corresponde a la segunda división";
-        } elseif ($nota >= 33) {
+        } elseif ($this->nota >= 33) {
             return "Corresponde a la tercera división";
         } else {
             return "Tienes que volver a repetir";
         }
     }
 }
-
 ?>
--- Nivel 1/Ejercicio 2/tests/GradoEstudioTests.php
+++ Nivel 1/Ejercicio 2/tests/GradoEstudioTests.php
@@ -1,20 +1,19 @@
 <?php
+include 'GradoEstudio.php';
 use PHPUnit\Framework\TestCase;
 
 class GradoEstudioTest extends TestCase {
     private $nota;
 
-    public function setUp(): void { //este metodo prepara los test unitarios instanciando cada vez
+    public function setUp(): void {
         $this->nota = new GradoEstudio(0);
     }
 
     public function testVerificarGradoEstudio() {
-        $this->assertEquals("Corresponde a la primera división", $this->nota->CalcularGrado_Estudio(79));
-        $this->assertEquals("Corresponde a la segunda división", $this->nota->CalcularGrado_Estudio(50));
-        $this->assertEquals("Corresponde a la tercera división", $this->nota->CalcularGrado_Estudio(33));
-        $this->assertEquals("Tienes que volver a repetir", $this->nota->CalcularGrado_Estudio(14));
+        $this->assertEquals("Corresponde a la primera división", $this->nota->CalcularGrado_Estudio());
+        $this->assertEquals("Corresponde a la segunda división", $this->nota->CalcularGrado_Estudio());
+        $this->assertEquals("Corresponde a la tercera división", $this->nota->CalcularGrado_Estudio());
+        $this->assertEquals("Tienes que volver a repetir", $this->nota->CalcularGrado_Estudio());
     }
 }
-
-
 ?>
